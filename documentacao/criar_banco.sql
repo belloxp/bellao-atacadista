3660bc6b977ebedb235606fa9385be6c5f72d977
Bello <guiilhermebello2@gmail.com>
2026-08-10 19:41:09 -0300

    Delete documentacao/criar_banco.sql

--- documentacao/criar_banco.sql
+++
@@ -1,8 +0,0 @@
--- Criação do banco de dados do Sistema de Compras Interno
-CREATE DATABASE IF NOT EXISTS db_compras;
-USE db_compras;
-
--- As tabelas base (usuarios, produtos, compras) são criadas pelo Sequelize
--- executando: node sync.js
--- As Views (vw_produtos_criticos, vw_volume_compras) são criadas
--- executando: node criarViews.js
